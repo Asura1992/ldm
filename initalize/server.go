eb61cf9253b24f849cab19d1462fc9b8d2201c49
laodaming <1059642220@qq.com>
2022-10-21 16:22:11 +0800

    fix

--- initalize/server.go
+++ initalize/server.go
@@ -2,7 +2,6 @@ package initalize
 
 import (
 	"github.com/go-micro/plugins/v4/registry/etcd"
-	grpcsvr "github.com/go-micro/plugins/v4/server/grpc"
 	"go-micro.dev/v4"
 	"go-micro.dev/v4/registry"
 	"ldm/common/config"
@@ -12,7 +11,7 @@ import (
 
 func InitService(srvName string) micro.Service {
 	service := micro.NewService(
-		micro.Server(grpcsvr.NewServer()),//这个要加上，不然grpc网关路由调用不会等待返回
+		//micro.Server(grpcsvr.NewServer()),//这个要加上，不然grpc网关路由调用不会等待返回
 		micro.Name(srvName),
 		micro.RegisterInterval(time.Second * 15),
 		micro.RegisterTTL(time.Second * 30),
