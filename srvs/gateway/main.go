3e769929b09a26b6a66dc48cceef18da7952596d
laodaming <1059642220@qq.com>
2022-10-21 18:39:48 +0800

    fix

--- srvs/gateway/main.go
+++ srvs/gateway/main.go
@@ -20,16 +20,9 @@ import (
 	"strings"
 	"time"
 )
-//响应结构体
-type grpcErrRsp struct {
-	Code 	int 			`json:"code"`
-	Message string 			`json:"message"`
-}
 var mux = runtime.NewServeMux(
 	//允许所有头信息
 	runtime.WithIncomingHeaderMatcher(allowHeader),
-	//错误响应组装器
-	runtime.WithErrorHandler(errResponseBuilder),
 	runtime.WithMarshalerOption(
 	runtime.MIMEWildcard,
 	&runtime.JSONPb{
@@ -42,23 +35,6 @@ var mux = runtime.NewServeMux(
 			DiscardUnknown: true, //忽略传入非定义的字段
 		},
 	}))
-
-//失败请求响应组装器
-func errResponseBuilder(ctx context.Context, serveMux *runtime.ServeMux, marshaler runtime.Marshaler, writer http.ResponseWriter, request *http.Request, err error) {
-	rsp := grpcErrRsp{
-		Code: -1,
-		Message: strings.ReplaceAll(err.Error(),"rpc error: code = Unknown desc = ",""),
-	}
-	b,err := marshaler.Marshal(rsp)
-	if err != nil{
-		log.Println("错误响应编码失败:",err.Error())
-		return
-	}
-	_,err = writer.Write(b)
-	if err != nil{
-		log.Println("响应输出失败:",err.Error())
-	}
-}
 //允许哪些自定义头信息
 func allowHeader(s string) (string, bool) {
 	switch s {
--- srvs/project/impl/project.go
+++ srvs/project/impl/project.go
@@ -2,12 +2,13 @@ package impl
 
 import (
 	"context"
-	"errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"ldm/common/protos/project"
 )
 
 //获取项目
 func (h ProjectImpl) GetProject(ctx context.Context, req *project.GetProjectReq, rsp *project.GetProjectRsp) error {
 	rsp.Msg = "project" + req.Name
-	return errors.New("hahahah")
+	return status.New(codes.InvalidArgument,"hahahah").Err()
 }
