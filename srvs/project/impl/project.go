6fc1805331c97997a68556b33eb7bd809eb31f3c
laodaming <1059642220@qq.com>
2022-10-21 19:13:34 +0800

    fix

--- srvs/project/impl/project.go
+++ srvs/project/impl/project.go
@@ -2,13 +2,13 @@ package impl
 
 import (
 	"context"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"errors"
 	"ldm/common/protos/project"
 )
 
 //获取项目
 func (h ProjectImpl) GetProject(ctx context.Context, req *project.GetProjectReq, rsp *project.GetProjectRsp) error {
 	rsp.Msg = "project" + req.Name
-	return status.New(codes.InvalidArgument,"hahahah").Err()
+	return errors.New("我错了")
 }
+
