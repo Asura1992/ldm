b03a89a6c07b0bbbad73dc008f7be0e60605d379
laodaming <1059642220@qq.com>
2022-10-22 16:38:03 +0800

    fix

--- srvs/project/impl/project.go
+++ srvs/project/impl/project.go
@@ -2,11 +2,13 @@ package impl
 
 import (
 	"context"
+	"fmt"
 	"ldm/common/protos/project"
 )
 
 //获取项目
 func (p ProjectImpl) GetProject(ctx context.Context, req *project.GetProjectReq, rsp *project.GetProjectRsp) error {
 	rsp.Msg = "project" + req.Name
+	fmt.Println("hahahaha")
 	return nil
 }
