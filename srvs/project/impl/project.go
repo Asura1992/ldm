e4d654355d04806e063388c3e5a76825b481ebe1
laodaming <1059642220@qq.com>
2022-10-25 14:14:50 +0800

    fix

--- srvs/project/impl/project.go
+++ srvs/project/impl/project.go
@@ -2,14 +2,11 @@ package impl
 
 import (
 	"context"
-	"fmt"
-	"go-micro.dev/v4/metadata"
 	"ldm/common/protos/project"
 )
 
 //获取项目
 func (p ProjectImpl) GetProject(ctx context.Context, req *project.GetProjectReq, rsp *project.GetProjectRsp) error {
 	rsp.Msg = "地瓜" + req.Name
-	fmt.Println(metadata.FromContext(ctx))
 	return nil
 }
