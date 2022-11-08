a61de0a3113d49ea96f7e93491f857ff424adb8a
laodaming <1059642220@qq.com>
2022-11-08 17:42:56 +0800

    fix

--- srvs/project/srv/project.go
+++ srvs/project/srv/project.go
@@ -2,6 +2,7 @@ package srv
 
 import (
 	"context"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"ldm/common/constant"
 	"ldm/common/protos/hello"
 	"ldm/common/protos/project"
@@ -11,5 +12,9 @@ import (
 func (p *ProjectImpl) GetProject(ctx context.Context, req *project.GetProjectReq, rsp *project.GetProjectRsp) error {
 	r, _ := hello.NewHelloService(constant.API_HELLO_SRV, p.client).Hello(ctx, &hello.HelloReq{Name: req.Name})
 	rsp.Msg = r.Msg
+	return nil
+}
+
+func (p *ProjectImpl) DelProject(ctx context.Context, empty *emptypb.Empty, empty2 *emptypb.Empty) error {
 	return nil
 }
