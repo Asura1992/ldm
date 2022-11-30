8d94ec9cc8b84a1658f0a918b9b17322ae7e9ba5
laodaming <1059642220@qq.com>
2022-11-30 14:34:40 +0800

    fix

--- srvs/liveroom/srv/liveroom.go
+++ srvs/liveroom/srv/liveroom.go
@@ -2,12 +2,10 @@ package srv
 
 import (
 	"context"
-	"fmt"
 	"ldm/common/protos/liveroom"
 )
 
 func (l LiveroomImpl) GetLiveroom(ctx context.Context, req *liveroom.GetLiveroomReq, rsp *liveroom.GetLiveroomRsp) error {
 	rsp.Msg = "哈哈哈哈" + req.Name
-	fmt.Println("11111111111111")
 	return nil
 }
